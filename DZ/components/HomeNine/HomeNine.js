1d7cd9a265bcb02147de799c5be34e93738d56fa
fei <1666903528@qq.com>
2019-05-08 21:01:53 +0800

    地理

--- DZ/components/HomeNine/HomeNine.js
+++ DZ/components/HomeNine/HomeNine.js
@@ -18,6 +18,33 @@ Component({
    * 组件的方法列表
    */
   methods: {
+    dianji:function(){
+      var me = this;
+      wx.getLocation({
+        type: 'wgs84',
+        altitude:true,
+        success: function(res) {
+          console.log(res)
+          var latitude = res.latitude
+          var longitude = res.longitude
+
+          me.loadCity(longitude, latitude);
+
+          // wx.showModal({
+          //   title: '当前位置',
+          //   content: '纬度:' + latitude + '经度' + longitude,
+          // })
+        },
+        // loadCity:function(lo,la){
+        //   console.log(la,lo)
+        // }
+      })
 
   }
+
+  }
+ 
 })
+
+
+// 16205384
--- DZ/pages/index/index.js
+++ DZ/pages/index/index.js
@@ -16,33 +16,74 @@ Page({
     })
   },
   onLoad: function () {
-    if (app.globalData.userInfo) {
-      this.setData({
-        userInfo: app.globalData.userInfo,
-        hasUserInfo: true
-      })
-    } else if (this.data.canIUse){
-      // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
-      // 所以此处加入 callback 以防止这种情况
-      app.userInfoReadyCallback = res => {
-        this.setData({
-          userInfo: res.userInfo,
-          hasUserInfo: true
-        })
-      }
-    } else {
-      // 在没有 open-type=getUserInfo 版本的兼容处理
-      wx.getUserInfo({
-        success: res => {
-          app.globalData.userInfo = res.userInfo
-          this.setData({
-            userInfo: res.userInfo,
-            hasUserInfo: true
-          })
-        }
-      })
-    }
+    // if (app.globalData.userInfo) {
+    //   this.setData({
+    //     userInfo: app.globalData.userInfo,
+    //     hasUserInfo: true
+    //   })
+    // } else if (this.data.canIUse){
+    //   // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
+    //   // 所以此处加入 callback 以防止这种情况
+    //   app.userInfoReadyCallback = res => {
+    //     this.setData({
+    //       userInfo: res.userInfo,
+    //       hasUserInfo: true
+    //     })
+    //   }
+    // } else {
+    //   // 在没有 open-type=getUserInfo 版本的兼容处理
+    //   wx.getUserInfo({
+    //     success: res => {
+    //       app.globalData.userInfo = res.userInfo
+    //       this.setData({
+    //         userInfo: res.userInfo,
+    //         hasUserInfo: true
+    //       })
+    //     }
+    //   })
+    // }
+
+    var me = this;
+    wx.getLocation({
+      // type:'wgs84',
+      altitude:true,
+      success: function(res) {
+        console.log(res)
+        var long = res.longitude
+        var lat = res.latitude
+        me.loadCity(long, lat)
+      },
+    })
+
+
   },
+
+
+    loadCity: function(long, lat){
+        var me = this;
+        wx.request({
+          url: 'https://api.map.baidu.com/geocoder/v2/?ak=CktTRenmdTUMPgXhCKDNQuzwZ9hWDcfW&location=' + lat + ',' + long + '&output=json',
+          data:{},
+          header:{
+            'Content-Type':'application/json'
+          },
+          success:function(res){
+            if(res && res.data){
+              var city = res.data.result.addressComponent.city;
+              console.log(res)
+              me.setData({
+                city:city.indexOf('市') >-1? city.substr(0,city.indexOf('市')) :city
+              });
+            }else{
+              me.setData({
+                city:'获取失败'
+              })
+            }
+          }
+        })
+    },
+
+
   getUserInfo: function(e) {
     console.log(e)
     app.globalData.userInfo = e.detail.userInfo
